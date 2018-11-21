b85252293ee5791b57e38192b12030bf514130db
Jacob Coughenour <jacob15545@yahoo.com>
2018-11-21 02:20:12 -0500

    fix socketio client trying to connect to prod with localhost

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,9 @@ class App extends React.Component {
 
 	componentDidMount() {
 
-		const socket = openSocket('localhost:8000');
+		let loc = window.location.origin.replace(/^http/, 'ws');
+
+		const socket = openSocket(loc.slice(0, loc.length - 4) + '8000');
 
 		socket.on('statesync', (data) => {
 			console.log("statesync", data);
