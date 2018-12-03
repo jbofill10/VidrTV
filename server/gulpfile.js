5e84181e845f81cac32b5aad166ab2df90f543c6
Jacob Coughenour <jacob15545@yahoo.com>
2018-12-02 20:31:10 -0500

    server-dev script uses gulp now
    implements #3

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -2,17 +2,20 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 
-exports.default = () => {
-
-	return gulp.src('src/**/*.js')
-
+const lint = () =>
+	gulp
+		.src('src/**/*.js')
 		// eslint
 		.pipe(eslint())
 		.pipe(eslint.format())
-		.pipe(eslint.failOnError())
+		.pipe(eslint.failOnError());
 
+const compile = () =>
+	gulp
+		.src('src/**/*.js')
 		// babel
 		.pipe(babel())
-
+		// output to build dir
 		.pipe(gulp.dest('build'));
-};
+
+exports.default = exports.build = gulp.parallel(lint, compile);
