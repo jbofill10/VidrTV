2b00185a1d4ab0119d1fc25edd909934cde27374
Jacob Coughenour <jacob15545@yahoo.com>
2019-01-29 23:06:58 -0500

    more env script

--- scripts/env.js
+++ scripts/env.js
@@ -8,97 +8,79 @@ const userConfigName = 'env.config';
 const userConfigPath = __dirname + '/../' + userConfigName;
 const syncedConfigPath = __dirname + '/synced.config';
 
-// command input
+var syncedConfig = {};
+var userConfig = {};
 
-// has args
-if (argv._.length > 0) {
-	switch (argv._[0]) {
-		case 'setup':
-			setup();
-			break;
-		default:
-			check();
-			break;
-	}
-} else {
-	// default to check
-	check();
-}
-
-function setup(newfile = true) {
-
-	// TODO: go through env config wizard
-
-	fs.writeFile(configPath, JSON.stringify(defaultconfig, null, 4), 'utf8', (err) => {
-		if (err) {
-			fancy.error(`An error occured while writing ${userConfigName} to ${userConfigPath}`);
-			return console.log(err);
-		} else
-			fancy.event(`Created ${userConfigName} file`);
-	});
-}
-
-function check() {
-	// check for user config
-	// if none, go to setup
-
-	fancy.has()
-
-	// see if config file exists
-	const hasConfig = fs.existsSync(userConfigPath);
-
-	fancy.has(hasConfig, 'has env.config', "doesn't have env.config");
-
-	if (!hasConfig) {
-		setup();
-		return;
-	}
-
-	// check if user config has all keys
-	// if some are missing, go to setup
-
-	// check if .env file have key(s) not in synced config
-	// if so, have them properly add keys via add command
-
-	// write .env files
-}
-
-
-
-
-
-
-
-
-const prompt = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-
-console.log(argv._);
-
+// load synced config
 fs.readFile(syncedConfigPath, { encoding: 'utf-8' }, (err, data) => {
 	if (err) {
-		fancy.error("An error occured when loading " + syncedConfigPath);
+		fancy.error(`An error occurred when trying to read ${syncedConfigPath}`);
 		console.error(err);
 	} else {
-		console.log(data);
+		syncedConfig = JSON.parse(data);
+		start();
 	}
 });
 
+function start() {
+	// command input
+	console.log(argv._[0]);
 
+	// has args
+	if (argv._.length > 0) {
+		if (commands.hasOwnProperty(argv._[0])) commands[argv._[0]]();
+		else
+			// invalid command
+			commands.check();
+	} else {
+		// default to check
+		commands.check();
+	}
+}
 
-fs.readFile(userConfigPath, { encoding: 'utf-8' }, (err, data) => {
-	if (err) {
-		fancy.error(`An error occured while writing ${userConfigName} to ${userConfigPath}`);
-		return console.log(err);
-	} else
-		fancy.event(`Created ${userConfigName} file`);
-});
+const commands = {
+	setup: (newfile = true) => {
+		// TODO: go through env config wizard
 
-// TODO: if there are missing keys, go through wizard to add them
+		const prompt = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
 
-// TODO: if there are new keys in .env files, tell user to add the properly via command
+		try {
+			//JSON.stringify({ test: 'hello' }, null, 4)
+			fs.writeFileSync(userConfigPath, 'test2', { encoding: 'utf8' });
+			fancy.event(`Created ${userConfigName} file`);
+		} catch (error) {
+			fancy.error(`An error occured while writing ${userConfigName} to ${userConfigPath}`);
+			return console.error(error);
+		}
+	},
 
-// TODO: write .env files
+	check: () => {
+		// see if config file exists
+		const hasConfig = fs.existsSync(userConfigPath);
 
+		if (!hasConfig) {
+			commands.setup();
+			return;
+		}
+
+		try {
+			userconfig = fs.readFileSync(userConfigPath, { encoding: 'utf-8' });
+		} catch (error) {
+			fancy.error(`An error occured while reading ${userConfigName} from ${userConfigPath}`);
+			return console.error(error);
+		}
+
+		// syncedConfig.env.keys;
+
+		// check if user config has all keys
+		// if some are missing, go to setup
+
+		// check if .env file have key(s) not in synced config
+		// if so, have them properly add keys via add command
+
+		// write .env files
+	}
+};
