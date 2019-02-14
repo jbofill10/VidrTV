6694466bc91ee21cb98fac52f7d108c7c5a63204
Jacob Coughenour <jacob15545@yahoo.com>
2019-02-13 21:26:50 -0500

    add header to generated .env file

--- lookenv.config.js
+++ lookenv.config.js
@@ -1,7 +1,7 @@
-const Joi = require('joi');
-const fs = require('fs');
-const path = require('path');
-const fancy = require('./scripts/fancy');
+const Joi = require("joi");
+const fs = require("fs");
+const path = require("path");
+const fancy = require("./scripts/fancy");
 
 const requirements = Joi.object().keys({
 	REACT_APP_GOOGLE_API_KEY: Joi.string().alphanum().length(39).required(),
@@ -10,15 +10,28 @@ const requirements = Joi.object().keys({
 });
 
 // create .env file if it doesn't exist
-if (process.env.NODE_ENV !== 'production' && !fs.existsSync(path.resolve(__dirname, '.env'))) {
+if (process.env.NODE_ENV !== "production" && !fs.existsSync(path.resolve(__dirname, ".env"))) {
+	const header = `#######################################################
+###     THIS FILE CONTAINS SENSITIVE INFORMATION    ###
+### DO NOT SHARE WITH ANYONE OUTSIDE OF THE PROJECT ###
+#######################################################
+
+### Project Environment Variables
+###
+### NOTE: add new entries to the requirements in lookenv.config.js
+`;
+
 	fancy.space();
-	fancy.warn('.env file not found!');
-	fancy.info('Creating a new one...');
+	fancy.space();
+	fancy.info(".env file not found!  Creating a new .env file!");
+	fancy.space();
+	fancy.warn("GET THE KEYS FROM SOMEONE WHO ALREADY HAS THEM !!!");
+	fancy.warn("THE PROJECT WILL NOT FUNCTION WITHOUT THEM !!!");
 	fancy.space();
 
 	fs.writeFileSync(
-		path.resolve(__dirname, '.env'),
-		requirements._inner.children.reduce((file, e) => (file += e.key + '=\n'), '')
+		path.resolve(__dirname, ".env"),
+		requirements._inner.children.reduce((file, e) => (file += e.key + "=\n"), header + "\n")
 	);
 }
 
