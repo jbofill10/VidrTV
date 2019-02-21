c21f77a0cb4d2bd5be814be480ae09ae399175a9
Jacob Coughenour <jacob15545@yahoo.com>
2019-02-20 22:46:58 -0500

    responsive aspect ratio

--- client/src/components/MediaPlayer.js
+++ client/src/components/MediaPlayer.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Radium from "radium";
 import { default as MediaControls, StatsOverlay } from "./MediaControls";
 import YouTubePlayer from "react-player/lib/players/YouTube";
 
 const paddingHack = 200;
-const PlaybackDeltaThreshold = 1000;
+const PlaybackDeltaThreshold = 3000;
 
-export default class MediaPlayer extends Component {
+@Radium
+class MediaPlayer extends Component {
 	constructor(props) {
 		super(props);
 
@@ -22,7 +24,8 @@ export default class MediaPlayer extends Component {
 			playbackRate: 1.0,
 			loop: false,
 			seeking: false,
-			showStats: true
+			showStats: true,
+			aspect: 9 / 16
 		};
 	}
 
@@ -98,6 +101,8 @@ export default class MediaPlayer extends Component {
 				}
 			}
 		});
+
+		// this.setState({ heightdiff: window.innerHeight - this._parentdiv.clientHeight });
 	}
 
 	ref = player => {
@@ -190,64 +195,95 @@ export default class MediaPlayer extends Component {
 		return (
 			<div
 				className={this.props.className}
-				style={{
-					border: "solid 1px",
-					borderRadius: "1px",
-					borderColor: "#c78bff",
-					boxShadow: "0 0 32px #c78bff75",
-					background: "rgba(255, 255, 255, 0.082)"
-				}}
+				style={[
+					{
+						border: "solid 1px #c78bff",
+						borderRadius: "1px",
+						// boxShadow: "0 0 32px #c78bff75",
+						background: "rgba(255, 255, 255, 0.082)",
+						position: "relative",
+						// maxHeight: `calc((${heightdiff | 0}px) * 9 / 16)`,
+						// height: "200px",
+						width: "100%",
+						overflow: "hidden",
+						boxSizing: "border-box"
+					},
+					this.props.style
+				]}
+				ref={div => (this._parentdiv = div)}
 			>
 				<div
-					className="media"
 					style={{
 						position: "relative",
-						display: "grid",
-						height: this.props.height,
-						width: this.props.width,
+						height: "100%",
+						width: "100%",
+						margin: "auto",
+						maxWidth: `calc((100vh - ${this.props.heightdiff |
+							0}px) * 16 / 9)`,
 						overflow: "hidden"
 					}}
 				>
+					{/* <div
+					style={{
+						position: "absolute",
+						top: "50%",
+						transform: "translateY(-50%)",
+						width: "100%",
+						overflow: "hidden"
+					}}
+				> */}
 					<div
+						className="media-wrapper"
 						style={{
-							position: "absolute",
-							top: -paddingHack,
-							pointerEvents: "none",
-							height: this.props.height + paddingHack * 2,
-							width: this.props.width
+							position: "relative",
+							paddingBottom: "calc(100% * 9 / 16)"
 						}}
 					>
-						<YouTubePlayer
-							ref={this.ref}
-							url={"http://www.youtube.com/watch?v=" + url}
-							pip={pip}
-							playing={playing}
-							loop={loop}
-							playbackRate={playbackRate}
-							volume={volume}
-							muted={muted}
-							width="100%"
-							height="100%"
-							progressInterval={200}
-							onReady={this.onReady}
-							onStart={this.onStart}
-							onPlay={this.onPlay}
-							onProgress={this.onProgress}
-							onDuration={this.onDuration}
-							onBuffer={this.onBuffer}
-							onSeek={this.onSeek}
-							onEnded={this.onEnded}
-							onError={this.onError}
-							config={{
-								youtube: {
-									playerVars: {
-										origin: window.location.href,
-										rel: null
-									}
-								}
+						<div
+							className="player-container"
+							style={{
+								position: "absolute",
+								top: -paddingHack,
+								bottom: -paddingHack,
+								width: "100%",
+								overflow: "hidden"
 							}}
-						/>
+						>
+							<YouTubePlayer
+								ref={this.ref}
+								url={"http://www.youtube.com/watch?v=" + url}
+								pip={pip}
+								playing={playing}
+								loop={loop}
+								playbackRate={playbackRate}
+								volume={volume}
+								muted={muted}
+								progressInterval={200}
+								onReady={this.onReady}
+								onStart={this.onStart}
+								onPlay={this.onPlay}
+								onProgress={this.onProgress}
+								onDuration={this.onDuration}
+								onBuffer={this.onBuffer}
+								onSeek={this.onSeek}
+								onEnded={this.onEnded}
+								onError={this.onError}
+								config={{
+									youtube: {
+										playerVars: {
+											origin: window.location.href,
+											rel: null
+										}
+									}
+								}}
+								height="100%"
+								width="100%"
+							/>
+						</div>
+						{/* </div> */}
 					</div>
+				</div>
+				<div className="player-overlay">
 					<MediaControls className="media-controls" player={this} />
 					{showStats && <StatsOverlay player={this} />}
 				</div>
@@ -255,3 +291,5 @@ export default class MediaPlayer extends Component {
 		);
 	}
 }
+
+export default MediaPlayer;
--- client/src/views/RoomView.js
+++ client/src/views/RoomView.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { MediaPlayer } from "../components";
+import { MediaPlayer, PlaylistView } from "../components";
 import openSocket from "socket.io-client";
+import { youtube } from "../youtube";
 
 export default class RoomView extends React.Component {
 	constructor(props) {
@@ -8,6 +9,7 @@ export default class RoomView extends React.Component {
 		this.roomid = props.match.params.id;
 		this.state = {
 			room: {},
+			mediacache: {},
 			joining: true
 		};
 
@@ -28,56 +30,149 @@ export default class RoomView extends React.Component {
 			console.log("fullsync", data);
 
 			this.setState({ room: data, joining: false });
+
+			let mediacache = {};
+
+			for (let i = 0; i < this.state.room.media.length; i++) {
+				if (!this._isMounted) break;
+				youtube
+					.getVideoByID(this.state.room.media[i])
+					.then(result => {
+						if (this._isMounted) {
+							mediacache[this.state.room.media[i]] = result;
+
+							this.setState({ mediacache: mediacache });
+						}
+					})
+					.catch(console.error);
+			}
 		});
+
+		this._isMounted = true;
 	}
 
 	componentWillUnmount() {
 		// disconnect from socket when roomview unmounts
 		this.socket.disconnect();
+
+		this._isMounted = false;
 	}
 
 	render() {
 		if (this.state.joining) return <div>Joining Room...</div>;
+
 		return (
 			<div
 				style={{
 					display: "flex",
 					overflow: "hidden",
-					textAlign: "center",
 					height: "100%",
-					width: "100%"
+					width: "100%",
+					maxWidth: "1920px",
+					margin: "0 auto"
 				}}
 			>
 				<main
 					style={{
-						overflowX: "hidden",
-						overflowY: "auto",
-						flex: 1
+						overflow: "hidden",
+						flex: 1,
+						display: "flex",
+						flexDirection: "column"
 					}}
 				>
-					<MediaPlayer
-						className="player-container"
-						width={640}
-						height={360}
-						socket={this.socket}
-						room={this.state.room}
-					/>
 					<div
 						style={{
-							background: "red",
-							height: 40
+							position: "relative",
+							height: "100%",
+							width: "100%",
+							flex: "0 1"
 						}}
 					>
-						<span>video info</span>
+						<MediaPlayer
+							className="player-container"
+							// player height + heightdiff = player height breakpoint
+							heightdiff={110}
+							socket={this.socket}
+							room={this.state.room}
+							mediacache={this.state.mediacache}
+						/>
+					</div>
+					<div
+						style={{
+							background: "rgb(44, 40, 52)",
+							height: 26,
+							padding: "14px 12px 12px 24px",
+							fontSize: 18
+							// flex: 2
+						}}
+					>
+						<div>
+							<span
+								style={{
+									color: "#FFF"
+								}}
+							>
+								{this.state.room.name}
+							</span>
+							<span
+								style={{
+									paddingLeft: 16,
+									color: "#aaa"
+								}}
+							>
+								{this.state.mediacache.hasOwnProperty(
+									this.state.room.media[this.state.room.cur]
+								)
+									? this.state.mediacache[
+											this.state.room.media[
+												this.state.room.cur
+											]
+									  ].title
+									: "Loading..."}
+							</span>
+						</div>
 					</div>
 				</main>
 				<div
 					style={{
-						background: "blue",
-						width: 320
+						background: "rgb(44, 40, 52)",
+						minWidth: 320,
+						maxWidth: 360,
+						display: "flex",
+						flexDirection: "column"
 					}}
 				>
-					sidebar
+					<div
+						style={{
+							background: "rgb(44, 40, 52)",
+							height: 40,
+							flexShrink: 0,
+							display: "flex"
+						}}
+					>
+						<button
+							style={{
+								flex: 1
+							}}
+						>
+							Chat
+						</button>
+						<button
+							style={{
+								flex: 1
+							}}
+						>
+							Playlist
+						</button>
+					</div>
+					<div
+						style={{
+							background: "rgb(0, 0, 0, 0.3)",
+							flex: 1
+						}}
+					>
+						sidebar
+					</div>
 				</div>
 			</div>
 		);
